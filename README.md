641685d898dff4c4708663d6c19428881560c534
SmKou <66178856+SmKou@users.noreply.github.com>
2025-07-26 00:43:14 -0700

    WIP Add index.html and style.css

---
+++ README.md
@@ -0,0 +1,10 @@
+# Coffee Masters service prototype
+
+Based on [Coffee Masters](/coffee-masters-mockup.png)
+
+## Details
+
+Process
+1. Products: View and select product(s)
+2. Cart: View and manage cart
+3. Confirm: Enter order
---
+++ src/main.js
@@ -0,0 +1,20 @@
+import './style.css'
+
+const sizes = ["Short (8 fl oz)", "Tall (12 fl oz)", "Grande (16 fl oz)", "Venti (20 fl oz)"]
+// const add_ons = {
+// 	main: {
+// 		hot: ["iced"],
+// 		foam: ["salted caramel", "peppermint", "strawberry", "lavendar"],
+// 		milk: ["dulce de leche", "sweetened condensed milk"],
+// 		non_dairy: ["oatmilk", "almond milk", "coconut milk"]
+// 	},
+// 	extra: {
+// 		spice: ["cardamom", "cinnamon", "nutmeg", "ginger", "allspice", "cayenne pepper"],
+// 		syrup: ["vanilla", "lavender", "pistachio", "maple", "pumpkin"],
+// 		extract: ["vanilla", "almond"],
+// 	},
+// 	preset: {
+// 		yak_
+// 	}
+// }
+const order = {}

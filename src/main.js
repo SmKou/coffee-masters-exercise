fa4d551b133f6cd9f0bc69107b3746db1c775a13
SmKou <66178856+SmKou@users.noreply.github.com>
2025-08-12 14:00:40 -0700

    Add product grid styling (no size btns or fn yet)

--- src/main.js
+++ src/main.js
@@ -1,20 +1,20 @@
 import './style.css'
 
-const sizes = ["Short (8 fl oz)", "Tall (12 fl oz)", "Grande (16 fl oz)", "Venti (20 fl oz)"]
-// const add_ons = {
-// 	main: {
-// 		hot: ["iced"],
-// 		foam: ["salted caramel", "peppermint", "strawberry", "lavendar"],
-// 		milk: ["dulce de leche", "sweetened condensed milk"],
-// 		non_dairy: ["oatmilk", "almond milk", "coconut milk"]
-// 	},
-// 	extra: {
-// 		spice: ["cardamom", "cinnamon", "nutmeg", "ginger", "allspice", "cayenne pepper"],
-// 		syrup: ["vanilla", "lavender", "pistachio", "maple", "pumpkin"],
-// 		extract: ["vanilla", "almond"],
-// 	},
-// 	preset: {
-// 		yak_
-// 	}
-// }
+const size = {
+	short: "Short (8 fl oz)",
+	tall: "Tall (12 fl oz)",
+	grande: "Grande (16 fl oz)",
+	venti: "Venti (20 fl oz)"
+}
+const amount = {
+	none: addon => "No " + addon,
+	little: addon => "Lite " + addon,
+	usual: addon => addon,
+	a_lot: addon => "Extra " + addon
+}
+
+document.querySelectorAll("#prod figure").forEach(fig => {
+	fig.addEventListener("click", e => {})
+})
+
 const order = {}

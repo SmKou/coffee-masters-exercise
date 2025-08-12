c3d13082825c3dc0d06dbe3a88e9bd5302434703
SmKou <66178856+SmKou@users.noreply.github.com>
2025-08-12 14:30:41 -0700

    WIP Add amounts to form

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ const size = {
 	grande: "Grande (16 fl oz)",
 	venti: "Venti (20 fl oz)"
 }
+
 const amount = {
 	none: addon => "No " + addon,
 	little: addon => "Lite " + addon,
@@ -13,8 +14,52 @@ const amount = {
 	a_lot: addon => "Extra " + addon
 }
 
+const selection = {
+	item: "",
+	size: "",
+	addons: ""
+}
+const order = {}
+
 document.querySelectorAll("#prod figure").forEach(fig => {
-	fig.addEventListener("click", e => {})
+	fig.addEventListener("click", e => {
+		const id = e.id
+		Object.keys(size).forEach(size_abbr => {
+			const btn = document.createElement("button")
+			btn.setAttribute("title", size[size_abbr])
+			btn.append(document.createTextNode(size_abbr))
+			btn.addEventListener("click", () => {
+				document.querySelector("#prod form").classList.remove("hidden")
+				selection.item = id
+				selection.size = size_abbr
+			})
+			document.querySelector(`#${id} .size-btns`).append(btn)
+		})
+	})
 })
 
-const order = {}
+const addon_amount = (name) => {
+	const field = document.createElement("fieldset")
+	const legend = document.createElement("legend")
+	legend.append(document.createTextNode("How much " + name + "?"))
+	field.append(legend)
+	Object.keys(amount).forEach(amt => {
+		const addon = amount[amt](name)
+		const ipt = document.createElement("input")
+		ipt.id = name + "_" + amt
+		ipt.type = "radio"
+		ipt.name = name + "_addon"
+
+		const label = document.createElement("label")
+		label.setAttribute("for", ipt.id)
+		label.append(document.createTextNode(addon))
+	})
+}
+
+document.querySelectorAll("#prod form details").forEach(details => {
+	details.addEventListener("change")
+})
+
+document.querySelector("#prod form").addEventListener("submit", e => {
+	e.preventDefault()
+})
